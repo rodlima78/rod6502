9f41a1b8c5bb24f413306a94a1e0b5547df565cf
Rodolfo Lima <rlima@nvidia.com>
2023-05-11 11:59:04 +0200

    rewrite of sys_malloc, a bit cleaner

--- bios/mem.s
+++ bios/mem.s
@@ -1,9 +1,11 @@
-.include "lcd.inc"
 .include "mem.inc"
 .include "io.inc"
+.include "sys.inc"
 
 .export init_mem
 .export fill_mem
+
+S = $0100
 
 .segment "HEAP"
 .align 2
@@ -17,163 +19,176 @@ len: .res 2
 ; ================================================================
 ; X: zp pointer to word where address will be written to
 ; Y: zp pointer to word w/ allocation size
-; ret C==1: error (nomem)
+; Pseudo-code:
+; bool align2 = size%2==0;
+; size += align2 ? 2 : 3;
+; byte *next;
+; for(ptr = heap_head; ptr != NULL; ptr = *ptr)
+; {
+;      if(*ptr & 1) // free ?
+;      {
+;           ptr &= ~1;
+;           int segsize = next-ptr;
+;           if(segsize >= size)
+;           {
+;               break;
+;           }
+;      }
+; }
+; assert(ptr != NULL);
+; output = ptr+2;
+; next = *ptr;
+; *ptr = ptr+size
+; ptr += size;
+; *ptr = next;
+; size -= align2 ? 2 : 3;
 sys_malloc:
     pha
-    phy
-    phx
 
-    ; add 2 (or 3) to allocation size to compensate the fact that the segment
-    ; size include its header size (2). Before returning we'll undo this.
-    ; If alignment!=2, add 3 instead to keep size multiple of 2.
+    ; Make sure we're aligned to 2 bytes -------------------------
+    ; size += size%2==0 ? 2 : 3 
     lda 0,y
-    lsr      ; C==1 ? align==1, or else, align==2
+    eor #1   ; flips bit0
+    lsr      ; C==0 ? align==1, or else, align==2
     php      ; save alignment info in carry for later retrieval
     lda #2
-    adc 0,y  ; LSB+2+Carry (align==1, add 3, or else add 2)
+    bcs @is_aligned
+    inc
+@is_aligned:
+    clc
+    adc 0,y  ; LSB+2+~Carry (align==1, add 3, or else add 2)
     sta 0,y
     bcc @skip_msb
     lda #0
-    adc 1,y  ; increment it (as C==1)
+    adc 1,y  ; increment size MSB (as C==1)
     sta 1,y
 @skip_msb:
 
+    phy
+    phx
+    SIZE   = S+2
+    tsx
+
     ; Loop through linked list of segments until we find one that
     ; has size greater of equal than what's needed
+
+    ; for(ptr = heap_head;
     lda #>heap_head ; ptr points at first segment
     sta ptr+1
     lda #<heap_head
     sta ptr
 
-    phx     ; save pointer to output address
-    tya
-    tax     ; now X == pointer to allocation size (so that we can reuse Y)
-
-    ldy #1  ; invariant, needed inside loop
+    ldy #1          ; loop invariant
 @loop:
+    ; if(*ptr & 1)
     lda (ptr)       ; load LSB of pointer to next segment (+ utilization flag on bit0) 
     lsr             
     bcs @found_free ; C==1 (bit0==1)? yes, found free segment
+    ; else
     asl
     pha             ; push LSB
-@loop_next:         ; expects LSB w/o flag on stack
-    lda (ptr),y     ; read MSB, we know y==1
+@loop_next:         ; expects LSB w/o flag on stack (expects y==1)
+    ; ptr = *ptr
+    lda (ptr),y     ; read MSB
     sta ptr+1       ; now we can change ptr, save MSB
     pla             ; pop LSB
     sta ptr         ; save LSB (we know bit0==0)
     bra @loop
 @found_free:
     ; test if end of heap
+    ; if ptr != NULL
     ; we know A == LSB>>1 of next address
     bne @test_size  ; LSB not zero? go on test seg size
     lda (ptr),y     ; LSB is zero, load up MSB now (we know y==1)
     bne @test_size  ; not zero? go see it it fits
-                    ; error, end of heap
-    ; restore stack
-    ply ; (not needed) restore ptr to output addr
-    plp ; restore whether len align==1 (C==1) or align==2
-    jsr @rollback_len_update
+    ; error, end of heap -----------
+    jsr sys_abort
+    .asciiz "ENOMEM"
 
-    jsr io_push_put_byte
-    .addr lcd_put_byte
-    jsr io_put_const_string
-    .asciiz "NOMEM"
-    jsr io_pop_put_byte
-    sec     ; indicate error
-    bra @end
 @test_size:
     ; calculate size of current segment
     lda (ptr)       ; next seg LSB
     and #.lobyte(~1); make sure LSB don't have the utilization flag set
-    pha             ; @loop_next needs next seg's LSB on top of stack
+    pha             ;
     sbc ptr         ; we know C==1, no need for sec
     lda (ptr),y     ; we know y==1
     sbc ptr+1       ; we only need the carry for sbc on MSB
-    cmp 1,x         ; seg len MSB < needed len MSB?
-    bcc @loop_next  ; yes, segment too small, try next one
+    ldy SIZE,x
+    cmp 1,y         ; seg len MSB < needed len MSB?
+    bcc @prepare_loop_next  ; yes, segment too small, try next one
     bne @found_fits ; seg len MSB > needed len MSB? yes, it fits
-    pla             ; restore LSB
+    pla             ; restore next seg LSB
     pha             ; still needs to be on stack for @loop_next
     sbc ptr         ; we know C==1, no need for sec
-    cmp 0,x         ; seg len LSB < needed len LSB?
+    cmp 0,y         ; seg len LSB < needed len LSB?
+@prepare_loop_next:
+    ldy #1          ; restore loop invariant
     bcc @loop_next  ; yes, seg too small, try next one
 @found_fits:        ; found segment large enough!
     pla             ; restore stack
-    ply             ; restore ptr to output address
 
-    ; return ptr+2 (skipping header)
+    ; output = ptr+2 (skip header)
     lda ptr
     clc
     adc #2
+    ply             ; restore ptr to output
     sta 0,y
     lda #0
     adc ptr+1
     sta 1,y
 
-    ; Y is now free for reuse
-
     ; Now we update the segment to indicate it's being used and create new empty segment -------
-
     ; save values that we'll write to the header of the new empty segment
+    ; next = *ptr
     ldy #1
     lda (ptr),y
     pha
     lda (ptr) ; bit0==1 as it's freed
     pha
 
-    ; mark segment as being occupied
-    and #.lobyte(~1) 
-    sta (ptr)
-
     ; make ptr point one past end of current segment (beginning of the new empty seg)
-    ; note: x currently points to len+2, exactly what we need
+    ldy SIZE,x
+    ; X is now free, can't use local stack from now on
+
+    ; note: 0,y currently points to len+2, exactly what we need
     ; and make cur seg point to next we'll create
+    ; *ptr = ptr+size
     clc
     lda ptr       
-    adc 0,x
+    adc 0,y
     pha
     lda ptr+1
-    adc 1,x
+    adc 1,y
+    tax
+    ldy #1
     sta (ptr),y
-    tay
     pla
     sta (ptr)
+    ; ptr += size
     sta ptr
-    sty ptr+1
-    
+    stx ptr+1
+
     ; create header of new (empty) segment
+    ; *ptr = next
     pla             ; restore LSB of the old next segment
     sta (ptr)       ; we know bit0==1
     pla
-    ldy #1
-    sta (ptr),y
+    sta (ptr),y     ; we know y==1
 
-    plp             ; restore alignment
-    jsr @rollback_len_update
-    clc
-
-@end:
-    plx
-    ply
-    pla
-    rts
-
-; pre-condition: C==1 ? align==1 : align==2
-; x = pointer to allocation size
-@rollback_len_update:
-    ; undo change in allocation size done at the beginning
+    ; size -= align2 ? 2 : 3
+    plx         ; restore ptr to size
+    plp         ; restore alignment
     lda 0,x
-    bcs @not_aligned
-    ina
-@not_aligned:
-    sec
-    sbc #3  ; if not aligned, will do LSB-3, or else, LSB+1-3 == LSB-2
+    sbc #2      ; C==0? size-3 : size-2
     sta 0,x
-    bcs @skip_msb3
-    lda #0
-    sbc 1,x ; decrement MSB (as C==0)
+    bcs @skip_msb2
+    lda 1,x
+    dea
     sta 1,x
-@skip_msb3:
+@skip_msb2:
+    clc     ; success
+
+    pla
     rts
 
 ; ================================================================
